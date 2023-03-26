63081eac55f1be2460ec1de2f6c38cb6a4dcea73
pardhualapati <pardhualapati01@gmail.com>
2023-03-26 14:31:52 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Online-Crop-Marketing
+India is a country that says that the farmers are the backbone of the country but still live in the 
+backward area of the country because they didn’t get adequate prices for their crops. The farmers are 
+getting suicide due to the low prices, which does not clear the debts that did for yielding the crop from 
+year to year the farmer's deaths increased across the country.
+Our idea is to provide an application interface for the farmer to directly interact with the buyer without the interference of the broker. In 
+this project, we are developing an app for registering both the farmers and the buyers. After 
+registration, the buyers can check the availability of needed crops across the country. The farmers can 
+compare the crop prices across the country and can get more benefits by using our application. The 
+farmers after registering you can check the crop prices across the different parts of the country. 
+Farmers can upload the quantity of their particular crop.
+The buyers can now the crop availability by selecting the particular crop and region. By selecting the crop and region the buyer can get notifications 
+of the farmer details they are available for that particular region and location. The buyer can 
+communicate with the farmer by contact details that were proposed by the notification. The farmers 
+can also able to know the prices across the country for their particular crops.
+
+<h1> Home Page</h1>
+<p align="center">
+  <img src="https://drive.google.com/uc?export=download&id=1nfKGdvzf3GyCpfc2dIJSm3MlAHL0mzfS" width="700" >
+</p>
+
+<h1> Register page </h1>
+
+<p align = "center">
+  <img src="https://drive.google.com/uc?export=download&id=15PAFv4zosAf73542LyCYHRadD74uH7DH" width="700" >
+  </p>
+  
+  <h1> Login page </h1>
+
+<p align = "center">
+  <img src="https://drive.google.com/uc?export=download&id=1IaZQyyaofHIBvI5QUO_Zl6sNFlRbe7uq" width="700" >
+  </p>
+  
+   <h1> Main Page After Login</h1>
+
+<p align = "center">
+  <img src="https://drive.google.com/uc?export=download&id=1ROI4dhL98Vm82kl2pZcpEYjuYH3eUqHS" width="700" >
+  </p>
+  
+   <h1> Compose Page </h1>
+
+<p align = "center">
+  <img src="https://drive.google.com/uc?export=download&id=1UY3gngUvHR9bNkXaNP0WsrGLcoQkUOCa" width="700" >
+  </p>
+  
+  <h1> Search Page </h1>
+
+<p align = "center">
+  <img src="https://drive.google.com/uc?export=download&id=1KjRrzJnxq1AcQNOuFMkyoB1Yau-8_EQ-" width="700" >
+  </p>
+
+<h1> Success Page(if farmer details found from existing data) </h1>
+
+<p align = "center">
+  <img src="https://drive.google.com/uc?export=download&id=1D3nrsnM9JZFka0Kbdk1R9A-FHQVM8IPO" width="700" >
+  </p>
+  
+  <h1> Failure Page(if farmer details not found from existing data) </h1>
+
+<p align = "center">
+  <img src="https://drive.google.com/uc?export=download&id=1WlPIRWoMXuLWWZ93NG5bdThlEKko8vni" width="750" >
+  </p>
+  
+  
---
+++ app.js
@@ -0,0 +1,277 @@
+//jshint esversion:6
+//require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const _ = require("lodash");
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const multer = require("multer");
+const fs = require('fs');
+const path = require('path');
+const router = express.Router();
+
+const app = express();
+
+app.use(express.static("public"));
+//ejs
+app.set('view engine', 'ejs');
+//body-parser
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+mongoose.connect( "mongodb+srv://pardhu:pardhu@onlinecropmarketing.5f2ndlt.mongodb.net/?retryWrites=true&w=majority", {
+  useNewUrlParser: true
+});
+// first created a simple schema and later implimented the new mwthod to create a new model of schema
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const User = new mongoose.model("User", userSchema);
+//Using the mongoose encryption
+//mongo schema for field data
+
+const postSchema = mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+    set: a => a === '' ? undefined : a
+  },
+  fieldEmail: {
+    type: String,
+    required: false,
+    set: b => b === '' ? undefined : b
+  },
+  phNo: {
+    type: String,
+    required: false,
+    set: c => c === '' ? undefined : c
+  },
+  state: {
+    type: String,
+    required: false,
+    set: d => d === '' ? undefined : d
+  },
+  zip: {
+    type: String,
+    required: false,
+    set: e => e === '' ? undefined : e
+  },
+  address: {
+    type: String,
+    required: false,
+    set: e => e === '' ? undefined : e
+  },
+
+  img:
+   {
+       data: Buffer,
+       contentType: String
+   },
+
+  cropName: {
+    type: String,
+    required:false,
+    set: f => f === '' ? undefined : f
+  },
+  cropregion: {
+    type: String,
+    required: false,
+    set: g => g === '' ? undefined : g
+  },
+  cropQuantity: {
+    type: String,
+    required: false,
+    set: h => h === '' ? undefined : h
+  },
+});
+
+// const Post = mongoose.model("Post", postSchema);
+
+const Post = new mongoose.model('Post', postSchema);
+
+
+
+// app.use(multer({ storage: fileStorage, fileFilter }).single("image"));
+
+
+app.get("/", function(req, res) {
+
+  res.render("home");
+
+});
+//storage
+
+
+app.get("/login", function(req, res) {
+
+  res.render("login");
+
+});
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+app.get("/main", function(req, res) {
+
+  res.render("main");
+})
+
+app.get("/marketprice", function(req, res) {
+
+  res.render("marketprice");
+});
+app.get("/aboutus", function(req, res) {
+
+  res.render("aboutus");
+});
+app.get("/logout", function(req, res) {
+
+  res.render("home");
+});
+app.get("/submit", function(req, res) {
+
+  res.render("compose");
+})
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
+app.get("/contactus", function(req, res) {
+  res.render("contactus");
+});
+
+
+app.get("/search", function(req, res) {
+
+  res.render("search");
+});
+
+
+
+
+
+
+// Step 6 - load the mongoose model for Image
+
+//var imgModel = require('./model');
+
+// var imgModel = require('./model');
+
+app.post("/register", function(req, res) {
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("main");
+      }
+    });
+  });
+
+});
+
+app.post("/login", function(req, res) {
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("main");
+          }
+        });
+
+      } else {
+        console.log("Password Not Correct");
+      }
+    }
+
+
+
+  });
+
+});
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+       cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+
+var upload = multer({ storage: storage });
+
+
+app.post("/compose", upload.single("image"), function(req, res) {
+
+  const post = new Post({
+    fullName: req.body.fullName,
+    fieldEmail: req.body.fieldEmail,
+    phNo: req.body.phnno,
+    state: req.body.state,
+    zip: req.body.pincode,
+    address: req.body.address,
+    img: {
+        data: fs.readFileSync(path.join(  __dirname + '/uploads/' + req.file.filename)),
+        contentType: 'image/png'
+        },
+    cropName: _.lowerCase(req.body.cropName),
+
+    cropregion: _.lowerCase(req.body.cropregion),
+    cropQuantity: req.body.cropQuantity
+  });
+
+  post.save();
+  res.redirect('/main');
+});
+
+
+app.post("/search", function(req, res) {
+
+  const cropname = _.lowerCase(req.body.cropName);
+  const regionarea = _.lowerCase(req.body.region);
+
+  Post.find({
+    cropName: cropname,
+    cropregion: regionarea
+  }, function(err, foundPost) {
+    if (err) {
+      console.log(err);
+    } else {
+
+      if(Object.keys(foundPost).length>0){
+
+  res.render("sucess" ,{users:foundPost});
+      }
+        else{
+  res.render("failure");
+          }
+
+    }
+  });
+
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started on Port 3000");
+});
